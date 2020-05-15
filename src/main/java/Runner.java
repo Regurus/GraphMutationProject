8d8db2ec80fa209ba04837ecb48134e4297a3a7c
Regurus <weltall2004@gmail.com>
2020-05-15 21:51:20 +0300

    UI added: no config tab, no functionality yet

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,48 @@
-public class Runner {
-    private static final double MUTATION_FACTOR = 0.2;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
 
+public class Runner extends Application{
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = FXMLLoader.load(getClass().getResource("fxmls/main.fxml"));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        primaryStage.setTitle("Graph Mutation Project Runner");
+        JMetro jMetro = new JMetro(Style.LIGHT);
+        Scene scene = new Scene(root,600,800);
+        jMetro.setScene(scene);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        MutatableGraph g = IOUtils.fileToGraph("test.csv");
-        System.out.println(11);
+        launch(args);
     }
 
 
