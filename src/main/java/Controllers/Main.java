f9bfef8d671b67e95a48ce8d929bad8e78c3d4b1
Regurus <weltall2004@gmail.com>
2020-05-16 15:57:02 +0300

    minor update

--- src/main/java/Controllers/Main.java
+++ src/main/java/Controllers/Main.java
@@ -1,4 +1,22 @@
 package Controllers;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Main {
+    @FXML
+    private TextField bar_a;
+    @FXML
+    private TextField bar_n;
+    @FXML
+    private TextField bar_m;
+
+    @FXML
+    private void toggleBarabasi(){
+        bar_a.setDisable(!bar_a.isDisabled());
+    }
 }
